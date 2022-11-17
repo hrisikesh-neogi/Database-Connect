238c2aecff1c402bd877ea450ca4899393346112
hrisikesh_neogi_ofc <104005791+hrisikeshOfc@users.noreply.github.com>
2022-11-17 09:31:53 +0000

    updated

--- pypi_pusher.sh
+++ pypi_pusher.sh
@@ -3,6 +3,8 @@ echo [$(date)]: "deleting existing dist files"
 rm -r dist
 echo [$(date)]: "deleting existing build files"
 rm -r build 
+echo [$(date)]: "Creating conda env with python 3.8"
+conda create --prefix ./env python=3.8 -y
 echo [$(date)]: "activate env"
 source activate ./env
 echo [$(date)]: "intalling setup script"
